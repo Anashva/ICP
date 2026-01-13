a772c240c5d1a93bb1d33263e527280ee24de486
Anashva Singh <anashva2406@gmail.com>
2026-01-13 13:02:51 +0530

    Add files via upload

---
+++ Week1/Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -0,0 +1,11 @@
+package Infinity_Champions_Program;
+
+import java.util.*;
+public class Find_the_Index_of_the_First_Occurrence_in_a_String {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String needle=sc.next();
+        System.out.println(s.indexOf(needle));
+    }
+}
